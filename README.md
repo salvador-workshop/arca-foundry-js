24f744f67ab3c61630320792a137ce9fa3e88681
RJ Salvador <salvador.workshop@gmail.com>
2026-04-28 13:42:50 -0400

    Config file working

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ Salvador Workshop's 3D [Code-CAD](https://github.com/Irev-Dev/curated-code-cad) 
 
 ## Usage
 
-### Building models with JSCAD
+### `npm run jscad`
 
 If a JSCAD project exists at `src/input/whateverProjectName/`, that can be built into an STL model by running:
 
 `npm run jscad --project="whateverProjectName"`
 
 The output model would then be located at `output/whateverProjectName/whateverProjectName.stl`
+
+### `npm run jscad-batch`
+
+The `jscad-batch` script relies on `arca.config.json` to run.
+
+- `buildProjects`: The project folders to build
+- `buildArgs`: A series of build options that are passed into each build project
--- src/build-jscad-batch.js
+++ src/build-jscad-batch.js
@@ -2,15 +2,30 @@
 // const fs = require('node:fs');
 
 import { exec } from "child_process";
-import fs from 'node:fs';
+import {
+    readFileSync,
+    lstatSync,
+    readdirSync,
+    existsSync,
+    mkdirSync,
+} from 'node:fs';
 import { dirname, join, relative } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const configPath = join(__dirname, '../arca.config.json')
+const inputDir = join(__dirname, '../input')
+const outputDir = join(__dirname, '../output')
+
+const arcaConfigJsonContent = readFileSync(configPath);
+const arcaConfigJsonData = JSON.parse(arcaConfigJsonContent);
+console.log(arcaConfigJsonContent);
+console.log(arcaConfigJsonData);
+console.log(typeof arcaConfigJsonData);
 
 const pEnv = process.env
-
 const inputProject = pEnv.npm_config_project ? pEnv.npm_config_project : ""
 let detectedProject = false
+let detectedBuildConfigs = false
 let errMsg = ''
 
 console.log('----------------------------------------------------------------\n')
@@ -25,10 +40,9 @@ if (!inputProject) {
 }
 
 const isDirectory = fileName => {
-    return fs.lstatSync(fileName).isDirectory();
+    return lstatSync(fileName).isDirectory();
 };
-const inputDir = join(__dirname, '../input')
-const directoryListLong = fs.readdirSync(inputDir)
+const directoryListLong = readdirSync(inputDir)
     .map(fileName => {
         return join(inputDir, fileName);
     })
@@ -40,38 +54,67 @@ const directoryList = directoryListLong.map(longDir => {
 console.log(directoryListLong)
 console.log(directoryList)
 
-if (directoryList.includes(projId)) {
+let bProjects = []
+let bArgs = []
+let configHasProjects = false
+let configHasBuildArgs = false
+
+if (Object.hasOwn(arcaConfigJsonData, 'buildProjects')
+    && Array.isArray(arcaConfigJsonData.buildProjects)
+    && arcaConfigJsonData.buildProjects.length > 0) {
+    configHasProjects = true
+    bProjects = arcaConfigJsonData.buildProjects
+}
+
+if (Object.hasOwn(arcaConfigJsonData, 'buildArgs')
+    && Array.isArray(arcaConfigJsonData.buildArgs)
+    && arcaConfigJsonData.buildArgs.length > 0) {
+    configHasBuildArgs = true
+    bArgs = arcaConfigJsonData.buildArgs
+}
+
+if (configHasProjects && configHasBuildArgs) {
+    detectedBuildConfigs = true
+}
+
+// if (directoryList.includes(projId)) {
+if (directoryList.some(r => bProjects.includes(r))) {
     detectedProject = true
 }
 
-if (detectedProject) {
-    const outputDir = join(__dirname, '../output')
-    const projOutputDir = join(outputDir, projId)
-    // const cmdString = `npx jscad src/input/${projId}/ -o output/${projId}/${projId}.stl`
-    const cmdString = `npx jscad input/${projId}/ -gp -o output/`
-
-    // create output folder
-    try {
-        if (!fs.existsSync(projOutputDir)) {
-            fs.mkdirSync(projOutputDir);
-        }
-    } catch (err) {
-        console.error(err);
-    }
-
+if (detectedProject && detectedBuildConfigs) {
     // run JSCAD
-    exec(cmdString, (err, stdout, stderr) => {
-        if (err) {
-            console.error();
-            console.error("Error:");
-            console.error(err);
-            console.error();
-        }
-        console.log(stdout);
-        console.error(stderr);
-    });
+    // exec(cmdString, (err, stdout, stderr) => {
+    //     if (err) {
+    //         console.error();
+    //         console.error("Error:");
+    //         console.error(err);
+    //         console.error();
+    //     }
+    //     console.log(stdout);
+    //     console.error(stderr);
+    // });
+
+    bProjects.forEach(bProject => {
+        console.log(`Building ${bProject}...`);
+        bArgs.forEach(bArgOpt => {
+            console.log('bArgOpt', bArgOpt);
+            const projOutputDir = join(outputDir, bProject)
+            // const cmdString = `npx jscad src/input/${bProject}/ -o output/${bProject}/${bProject}.stl`
+            const cmdString = `npx jscad input/${bProject}/ -gp -o output/`
+
+            // create output folder
+            try {
+                if (!existsSync(projOutputDir)) {
+                    mkdirSync(projOutputDir);
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        })
+    })
 } else {
-    errMsg = '> Project not found!'
+    errMsg = '> Project or build configuration not found!'
     console.error(errMsg)
 }
 
